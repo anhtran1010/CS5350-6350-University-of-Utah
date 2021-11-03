89787482161de36be4dae555b5273d7e4931479c
Anh Tran <anhb.tran1010@mail.com>
2021-11-02 19:23:46 -0600

    fix sh file typo

--- ML_hw/Perceptron/run.sh
+++ ML_hw/Perceptron/run.sh
@@ -1,2 +1,2 @@
 #to run the perceptron, voted perceptron, averaged perceptron and get the result reported in the pdf. Note, since for the Standard Perceptron, the data is shuffled every epoch, result maybe different (I got the shuffle from the lecture slide so don't come for me)
-python3 Perception.py
+python3 Perceptron.py
